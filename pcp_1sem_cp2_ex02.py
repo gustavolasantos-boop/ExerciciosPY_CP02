cab38623a09fdb020a0bc43deb32bb8c1d65d36f
LSHummel <lshummel13@gmail.com>
2026-04-17 10:55:25 -0300

    Correção do exercicio

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -1,32 +1,66 @@
-valor1 = int(input("Digite um numero:\n"))
-valor2 = int(input("Digite um numero:\n"))
-valor3 = int(input("Digite um numero:\n"))
+lado_1 = float(input("Medida do lado do triângulo: "))
+lado_2 = float(input("Medida do lado do triângulo: "))
+lado_3 = float(input("Medida do lado do triângulo: "))
 
-valores = [valor1,valor2,valor3]
 
-valores.sort(reverse=True)
+if lado_1 > lado_2 > lado_3:
+    A = lado_1
+    B = lado_2
+    C = lado_3
+elif lado_1 > lado_3 > lado_2:
+    A = lado_1
+    B = lado_3
+    C = lado_2
+elif lado_2 > lado_1 > lado_3:
+    A = lado_2
+    B = lado_1
+    C = lado_3
+elif lado_2 > lado_3 > lado_1:
+    A = lado_2
+    B = lado_3
+    C = lado_1
+elif lado_3 > lado_1 > lado_2:
+    A = lado_3
+    B = lado_1
+    C = lado_2
+elif lado_3 > lado_2 > lado_1:
+    A = lado_3
+    B = lado_2
+    C = lado_1
+elif lado_1 == lado_2 == lado_3 or (lado_3 == lado_2) < lado_1:
+    A = lado_1
+    B = lado_2
+    C = lado_3
+elif (lado_1 == lado_2) < lado_3 or (lado_1 == lado_3) > lado_2:
+    A = lado_3
+    B = lado_1
+    C = lado_2
+elif (lado_1 == lado_3) < lado_2 or (lado_1 == lado_2) > lado_3:
+    A = lado_2
+    B = lado_3
+    C = lado_1
+elif (lado_3 == lado_2) > lado_1:
+    A = lado_3
+    B = lado_2
+    C = lado_1
 
-A = valores[0]
-B = valores[1]
-C = valores[2]
 
-print(f"O valor de A é: {A}")
-print(f"O valor de B é: {B}")
-print(f"O valor de C é: {C}")
+print("Classificção do Triângulo com base nos ângulos")
+if A >= (B + C):
+    print("Os lados não formam um Triângulo\n")
+elif A**2 == (B**2 + C**2):
+    print("Os ângulos formam um Triângulo Retângulo\n")
+elif A**2 > (B**2 + C**2):
+    print("Os ângulos formam um Triângulo Obtusângulo\n")
+elif A**2 < (B**2 + C**2):
+    print("Os ângulos formam um Triângulo Acutângulo\n")
+else:
+    print("ERRO!")
 
-if A >= B+C:
-    print("NÃO FORMA TRIÂNGULO")
-elif A == B == C:
-    print("TRIÂNGULO EQUILÁTERO")
-elif A == B or A == C or B == C:
-    print("TRIÂNGULO ISÓSCELES")
-
-if A >= B+C:
-    print()
-elif  A^2 == B^2 + C^2:
-    print("TRIÂNGULO RETÂNGULO")
-elif A^2 > B^2 + C^2:
-    print("TRIÂNGULO OBTUSÂNGULO")
-elif A^2 < B^2 + C^2:
-    print("TRIÂNGULO ACUTÂNGULO")
-
+print("Classificação do Triângulo com base nos lados")
+if A == B == C:
+    print("Os lados do Triângulo formam um Triângulo Equilatero")
+elif A == B != C or A == C != B or B == C != A:
+    print("Os lados do Triângulo formam um Triângulo Isosceles")
+else:
+    print("Não possui classificação com base nos lados")
