cf3c47974c96fa9a910853a3f14b67dfe615bf8a
gabrielcggs <gabrielcggsilva@gmail.com>
2026-04-19 12:05:54 -0300

    passos 2 e 3 ( regras para aprovação e taxa de juros)

--- pcp_1sem_cp2_ex05.py
+++ pcp_1sem_cp2_ex05.py
@@ -3,3 +3,20 @@ idade = int(input("Digite a sua idade: "))
 renda = float(input("Informe sua renda mensal: "))
 emprestimo = float(input("Informe o valor que deseja retirar de emprestimo: "))
 parcelas = int(input("Número de parcelas que deseja: "))
+
+def pode_aprovar(idade, renda, emprestimo):
+    return idade >= 18 and emprestimo < 20*renda
+
+if parcelas < 3 or parcelas > 24:
+    print("Número de parcelas inválido, deve ser entre 3 e 24")
+else:
+    if not pode_aprovar(idade, renda, emprestimo):
+        print("Empréstimo negado!")
+    else:
+        def taxa(parcelas):
+            if parcelas < 7:
+                return 0.05
+            elif parcelas < 13:
+                return 0.08
+            else:
+                return 0.1
